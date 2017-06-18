e72546fff0b398eb9f3aa281d1b9d37dbac3a4c4
Lukas Rosenberger <lrosenberger.mmt-b2014@fh-salzburg.ac.at>
2017-06-18 20:49:17 +0200

    enabled tap gestures and fixed meta display bugs

--- Assets/MetaInformationRenderer.cs
+++ Assets/MetaInformationRenderer.cs
@@ -27,53 +27,18 @@ public class MetaInformationRenderer : MonoBehaviour
         StartCoroutine(initializePanel(bottomPanel, pageData.BottomElement));
         StartCoroutine(initializePanel(sidePanel, pageData.SideElement));
     }
-
     IEnumerator initializePanel(GameObject panel, ElementMetaData elementData)
     {
-
-        if(elementData.Type == ElementType.NONE)
+        Debug.Log("initializing panel?");
+        if (elementData.Type == ElementType.NONE)
         {
             panel.SetActive(false);
             yield break;
         }
         panel.SetActive(true);
-        panel.GetComponent<Renderer>().enabled = true;
-        panel.GetComponentInChildren<TextMesh>().text = "";
-        WWW www=null;
-        if (elementData.Type != ElementType.VIDEO && elementData.Type != ElementType.ASSET)
-        {
-            www = new WWW(elementData.Url);
-            yield return www;
-        }
-        switch (elementData.Type)
-        {
-            case ElementType.IMAGE:
-                panel.GetComponent<Renderer>().material.mainTexture = www.texture;
-                break;
-            case ElementType.AUDIO:
-                panel.GetComponentInChildren<TextMesh>().text = elementData.Url;
-                panel.GetComponent<AudioSource>().clip = www.GetAudioClip();
-                panel.GetComponent<AudioSource>().Play();
-                break;
-            case ElementType.VIDEO: //TODO find out why audio is not working
-                VideoPlayer vp = panel.GetComponent<VideoPlayer>();
-                AudioSource audios = panel.GetComponent<AudioSource>();
-                vp.url = elementData.Url;
-                vp.EnableAudioTrack(0, true);
-                vp.SetTargetAudioSource(0, audios);
-                vp.Prepare();
-                vp.Play();
-                audios.Play();
-                //panel.GetComponent<Renderer>().material.mainTexture = www.GetMovieTexture();
-                //panel.GetComponent<AudioSource>().clip = www.GetMovieTexture().audioClip;
-                break;
-            case ElementType.TEXT:
-                panel.GetComponentInChildren<TextMesh>().text = www.text;
-                break;
-            case ElementType.ASSET:
-                panel.GetComponent<MeshRenderer>().enabled = false;
-                FindObjectOfType<ARMediator>().LoadTrackedObject("redfox", elementData.Url, panel.transform);
-                break;
-        }
+        ElementInformationRenderer EIRenderer = panel.GetComponent<ElementInformationRenderer>();
+        yield return EIRenderer.initializePanel(elementData);
+
     }
+
 }
---
+++ Assets/Scripts/ElementInformationRenderer.cs
@@ -0,0 +1,123 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ElementInformationRenderer : MonoBehaviour, IInputClickHandler
+{
+    public bool activeElement = false;
+    public ElementMetaData currentMetaData;
+
+    public void Update()
+    {
+        if (currentMetaData.Type == ElementType.ASSET)
+            GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.LogError("I WAS CLICKED!");
+        activeElement = !activeElement;
+        if(activeElement)
+        {
+            switch (currentMetaData.Type)
+            {
+                case ElementType.IMAGE:
+                    break;
+                case ElementType.AUDIO:
+                    GetComponent<AudioSource>().Play();
+                    break;
+                case ElementType.VIDEO: //TODO find out why audio is not working
+                    VideoPlayer vp = GetComponent<VideoPlayer>();
+                    AudioSource audios = GetComponent<AudioSource>();
+                    vp.Play();
+                    audios.Play();
+                    break;
+                case ElementType.TEXT:
+                    //GetComponentInChildren<TextMesh>().text = www.text;
+                    break;
+                case ElementType.ASSET:
+                    //GetComponent<MeshRenderer>().enabled = false;
+                    //FindObjectOfType<ARMediator>().LoadTrackedObject("redfox", elementData.Url, transform);
+                    break;
+            }
+        }
+        else
+        {
+            switch (currentMetaData.Type)
+            {
+                case ElementType.IMAGE:
+                    break;
+                case ElementType.AUDIO:
+                    GetComponent<AudioSource>().Stop();
+                    break;
+                case ElementType.VIDEO: //TODO find out why audio is not working
+                    VideoPlayer vp = GetComponent<VideoPlayer>();
+                    AudioSource audios = GetComponent<AudioSource>();
+                    vp.Stop();
+                    audios.Stop();
+                    break;
+                case ElementType.TEXT:
+                    //GetComponentInChildren<TextMesh>().text = www.text;
+                    break;
+                case ElementType.ASSET:
+                    //GetComponent<MeshRenderer>().enabled = false;
+                    //FindObjectOfType<ARMediator>().LoadTrackedObject("redfox", elementData.Url, transform);
+                    break;
+            }
+        }
+    }
+
+    public IEnumerator initializePanel(ElementMetaData elementData)
+    {
+        cleanPanel();
+        currentMetaData = elementData;
+        WWW www = null;
+        if (elementData.Type != ElementType.VIDEO && elementData.Type != ElementType.ASSET)
+        {
+            www = new WWW(elementData.Url);
+            yield return www;
+        }
+        switch (elementData.Type)
+        {
+            case ElementType.IMAGE:
+                GetComponent<Renderer>().material.mainTexture = www.texture;
+                break;
+            case ElementType.AUDIO:
+                GetComponentInChildren<TextMesh>().text = elementData.Url;
+                GetComponent<AudioSource>().clip = www.GetAudioClip();
+                //GetComponent<AudioSource>().Play();
+                break;
+            case ElementType.VIDEO: //TODO find out why audio is not working
+                VideoPlayer vp = GetComponent<VideoPlayer>();
+                AudioSource audios = GetComponent<AudioSource>();
+                vp.url = elementData.Url;
+                vp.EnableAudioTrack(0, true);
+                vp.SetTargetAudioSource(0, audios);
+                vp.Prepare();
+                //vp.Play();
+                //audios.Play();
+                break;
+            case ElementType.TEXT:
+                GetComponentInChildren<TextMesh>().text = www.text;
+                break;
+            case ElementType.ASSET:
+                GetComponent<MeshRenderer>().enabled = false;
+                FindObjectOfType<ARMediator>().LoadTrackedObject("redfox", elementData.Url, transform);
+                break;
+        }
+    }
+
+    void cleanPanel()
+    {
+        activeElement = false;
+        GetComponent<Renderer>().enabled = true;
+        GetComponentInChildren<TextMesh>().text = "";
+        GetComponent<Renderer>().material.mainTexture = null;
+        GetComponent<AudioSource>().clip = null;
+        GetComponent<VideoPlayer>().clip = null;
+        if (transform.childCount>1)
+            Destroy(transform.GetChild(1).gameObject);
+    }
+}
